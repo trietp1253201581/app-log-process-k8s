4480dc4588b7014df4b417212851f35a24037538
Le Minh Triet <trietkd1475369@gmail.com>
2025-05-16 23:41:05 +0700

    Modify connection port of resource manager

--- docker-images/hadoop/scripts/entrypoint.sh
+++ docker-images/hadoop/scripts/entrypoint.sh
@@ -1,18 +1,16 @@
 #! /bin/bash
 
 HOSTNAME=$(hostname)
-service ssh start
 
 if [[ $HOSTNAME == *"namenode"* ]]; then
-    hdfs namenode -format -force
+    if [ ! -f /hadoop/dfs/name/current/VERSION ]; then
+        hdfs namenode -format -force;
+    fi;
     hdfs namenode
     tail -f $HADOOP_HOME/logs/*namenode*.log
 elif [[ $HOSTNAME == *"datanode"* ]]; then
     hdfs datanode
     tail -f $HADOOP_HOME/logs/*datanode*.log
-elif [[ $HOSTNAME == *"secondarynamenode"* ]]; then
-    hdfs secondarynamenode
-    tail -f $HADOOP_HOME/logs/*secondarynamenode*.log
 elif [[ $HOSTNAME == *"resourcemanager"* ]]; then
     yarn resourcemanager
     tail -f $HADOOP_HOME/logs/*resourcemanager*.log
