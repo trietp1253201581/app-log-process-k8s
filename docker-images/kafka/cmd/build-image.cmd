42ddfed2c6f533061226f73d2421af53d2224b5d
Le Minh Triet <trietkd1475369@gmail.com>
2025-05-17 23:50:57 +0700

    Remove server.properties and Modify Scripts

--- docker-images/kafka/cmd/build-image.cmd
+++ docker-images/kafka/cmd/build-image.cmd
@@ -4,7 +4,3 @@ set DIR=docker-images/kafka
 set LOCALDIR=tmp/custom-kafka.tar
 
 docker build -t %IMAGENAME% %DIR%  
-
-docker save %IMAGENAME% -o %LOCALDIR% 
-
-docker load -i %LOCALDIR%
--- docker-images/kafka/scripts/start-kafka.sh
+++ docker-images/kafka/scripts/start-kafka.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-export KAFKA_CFG_ZOOKEEPER_CONNECT=${KAFKA_ZOOKEEPER_CONNECT:-zookeeper:2181}
-export KAFKA_CFG_ADVERTISED_LISTENERS=${KAFKA_ADVERTISED_LISTENERS:-PLAINTEXT://kafka:9092}
-export KAFKA_CFG_LISTENERS=${KAFKA_LISTENERS:-PLAINTEXT://0.0.0.0:9092}
-
 exec /opt/bitnami/scripts/kafka/entrypoint.sh /opt/bitnami/scripts/kafka/run.sh
