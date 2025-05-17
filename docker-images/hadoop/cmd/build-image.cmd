b1a96d30e23e02d7d6ef2185dcb601588e196f61
Le Minh Triet <trietkd1475369@gmail.com>
2025-05-17 20:21:47 +0700

    Modify Build image CMD for hadoop

--- docker-images/hadoop/cmd/build-image.cmd
+++ docker-images/hadoop/cmd/build-image.cmd
@@ -1,9 +1,10 @@
 @echo off
 set IMAGENAME=custom-hadoop:latest
 set DIR=docker-images/hadoop
+set LOCALDIR=tmp/custom-hadoop.tar
 
 docker build -t %IMAGENAME% %DIR%  
 
-docker save %IMAGENAME% -o custom-hadoop.tar  
+docker save %IMAGENAME% -o %LOCALDIR% 
 
-docker load -i custom-hadoop.tar
+docker load -i %LOCALDIR%
