0eccf45f87475222b9eb00309ada530e13f23949
Le Minh Triet <bpscm2826@gmail.com>
2025-06-03 16:12:46 +0700

    Modify data schema

--- docker-images/application/app/sim_log.py
+++ docker-images/application/app/sim_log.py
@@ -20,26 +20,25 @@ producer = KafkaProducer(
 
 def simulate_log():
     return {
-        "timestamp": time.time(),
+        "event_time": time.time(),
         "user_id": faker.uuid4(),
         "action": random.choice(['click', 'view', 'purcharse', 'login', 'logout']),
         "device": faker.user_agent(),
         "location": faker.city()
     }
-    
+
 log_rate = config['generator']['log-rate']
 interval = 1.0 / log_rate
 kafka_topic = config['kafka']['topic']
 
 now_time = time.time()
-logs = []
+#logs = []
 while True:
     log = simulate_log()
-    logs.append(log)
+    #logs.append(log)
     try:
-        if time.time() - now_time >= 10:
-            producer.send(kafka_topic, value=logs)
-            logs.clear()
+        producer.send(kafka_topic, value=log)
+        print(f"Succesfully send log at {time.time():.0f}")
     except:
         print("Error when send to kafka topic")
     time.sleep(interval)
--- docker-images/spark/scripts/streaming_process.py
+++ docker-images/spark/scripts/streaming_process.py
@@ -1,11 +1,12 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import from_json, col
-from pyspark.sql.types import StructType, StringType, TimestampType
+from pyspark.sql.functions import from_json, col, from_unixtime
+from pyspark.sql.types import StructType, StringType, TimestampType, DoubleType
 
-spark = SparkSession.Builder().appName("Kafka2HDFS").getOrCreate()
+spark = SparkSession.Builder().appName("Kafka2HDFS")\
+    .config('spark.sql.parquet.outputTimestampType', 'INT96').getOrCreate()
 
 schema = StructType() \
-    .add("timestamp", TimestampType()) \
+    .add("event_time", DoubleType()) \
     .add("user_id", StringType()) \
     .add("action", StringType()) \
     .add("device", StringType()) \
@@ -15,18 +16,24 @@ df = spark.readStream \
     .format("kafka") \
     .option("kafka.bootstrap.servers", "kafka:9092") \
     .option("subscribe", "app-logs") \
-    .option("startingOffsets", "earliest") \
+    .option("startingOffsets", "latest") \
+    .option("failOnDataLoss", "false") \
     .load()
     
 json_df = df.selectExpr("CAST(value AS STRING)") \
     .select(from_json(col("value"), schema).alias("data")) \
-    .select("data.*")
+    .select("data.*") \
+    .withColumn("event_time", from_unixtime(col("event_time")).cast("timestamp"))
+    
+def write_to_parquet(batch_df, batch_id):
+    batch_df.write \
+        .mode("append") \
+        .option("timestampFormat", "yyyy-MM-dd HH:mm:ss") \
+        .parquet("hdfs://namenode:9000/applog/streaming")
 
 query = json_df.writeStream \
-    .format("parquet") \
-    .option("path", "hdfs://namenode:9000/output/logs") \
-    .option("checkpointLocation", "hdfs://namenode:9000/checkpoints/logs") \
-    .outputMode("append") \
+    .foreachBatch(write_to_parquet)\
+    .option("checkpointLocation", "hdfs://namenode:9000/checkpoints/logs-v2") \
     .start()
     
 query.awaitTermination()
